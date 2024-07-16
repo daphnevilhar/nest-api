9b60820fd8c1cf29e0e549f38da59c772f382259
Daphne Vilhar <daphnevilhar@Laptop-de-Daphne.local>
2024-07-15 21:21:23 -0300

    refact env config module

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,17 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import { join } from 'path';
+import { env } from 'process';
 import { EnvConfigService } from './env-config.service';
 
 @Module({
   providers: [EnvConfigService],
 })
-export class EnvConfigModule {}
+export class EnvConfigModule extends ConfigModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    return super.forRoot({
+      ...options,
+      envFilePath: [join(__dirname, `../../../../.env.${env.NODE_ENV}`)],
+    });
+  }
+}
